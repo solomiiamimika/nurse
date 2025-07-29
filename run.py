fb334ba5c585e54f4172442d96e5f62985802d49
Solomiia Vankovych <108820430+solomiiamimika@users.noreply.github.com>
2025-07-29 15:46:20 +0300

    ngrok localosting

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from app import create_app
 from app.extensions import db
 from app.models import User,Message,Service,Appointment,Payment,MedicalRecord,Prescription,Review
-
+from pyngrok import ngrok
+ngrok.set_auth_token('30Y5lts3TU8tBYOQ5g0CAxupy09_5M8qwijjkLXatMoCjpjbT')
 app = create_app()
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, ssl_context=None, debug=True)
+    URL=ngrok.connect(5000).public_url
+    print(URL)
+    app.run(port=5000)
